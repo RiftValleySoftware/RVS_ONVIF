f16d5b2f0dc34b98bde1d63d876361377bfe4714
Chris Marshall <chris@riftvalleysoftware.com>
2019-05-01 15:27:06 -0400

    OK. Got the current core "setters" done. Next, the "getters." I know they work, but I need to display the results.

--- RVS_ONVIF Mac Test Harness/RVS_ONVIF_Mac_Test_Harness_CoreDispatcher.swift
+++ RVS_ONVIF Mac Test Harness/RVS_ONVIF_Mac_Test_Harness_CoreDispatcher.swift
@@ -43,20 +43,20 @@ class RVS_ONVIF_Mac_Test_Harness_CoreDispatcher: RVS_ONVIF_Mac_Test_Harness_Disp
             
             switch inCommand.rawValue {
             case "SetHostname":
-                dataEntryDialog = RVS_ONVIF_Mac_Test_Harness_FunctionData_ViewController.dialogFactory(["Name": .textEntry(defaultValue: "", callback: hostNameCallback)], command: inCommand, dispatcher: self)
+                dataEntryDialog = RVS_ONVIF_Mac_Test_Harness_FunctionData_ViewController.dialogFactory(["Name": .textEntry(defaultValue: "", callback: nameCallback)], command: inCommand, dispatcher: self)
                 
             case "SetHostnameFromDHCP":
                 dataEntryDialog = RVS_ONVIF_Mac_Test_Harness_FunctionData_ViewController.dialogFactory(["FromDHCP": .pickOne(values: ["false", "true"], selectedIndex: 0, callback: fromDHCPCallback)], command: inCommand, dispatcher: self)
 
             case "SetNTP":
-                dataEntryDialog = RVS_ONVIF_Mac_Test_Harness_FunctionData_ViewController.dialogFactory(["FromDHCP": .pickOne(values: ["false", "true"], selectedIndex: 0, callback: fromDHCPCallback), "NTPManual": .textEntry(defaultValue: "", callback: ntpListCallback)], command: inCommand, dispatcher: self)
+                dataEntryDialog = RVS_ONVIF_Mac_Test_Harness_FunctionData_ViewController.dialogFactory(["FromDHCP": .pickOne(values: ["false", "true"], selectedIndex: 0, callback: fromDHCPCallback), "NTPManual": .textEntry(defaultValue: "", callback: ntpIPAddressListCallback)], command: inCommand, dispatcher: self)
 
             case "SetDNS":
-                ()
-                
+                dataEntryDialog = RVS_ONVIF_Mac_Test_Harness_FunctionData_ViewController.dialogFactory(["SearchDomain": .textEntry(defaultValue: "", callback: dnsSearchDomainCallback), "FromDHCP": .pickOne(values: ["false", "true"], selectedIndex: 0, callback: fromDHCPCallback), "DNSManual": .textEntry(defaultValue: "", callback: dnsIPAddressListCallback)], command: inCommand, dispatcher: self)
+
             case "SetDynamicDNS":
-                ()
-                
+                dataEntryDialog = RVS_ONVIF_Mac_Test_Harness_FunctionData_ViewController.dialogFactory(["Type": .pickOne(values: ["NoUpdate", "ServerUpdates", "ClientUpdates"], selectedIndex: 0, callback: dynDNSTypeCallback), "Name": .textEntry(defaultValue: "", callback: nameCallback), "TTL": .textEntry(defaultValue: "", callback: ttlCallback)], command: inCommand, dispatcher: self)
+
             default:
                 ()
             }
@@ -72,7 +72,14 @@ class RVS_ONVIF_Mac_Test_Harness_CoreDispatcher: RVS_ONVIF_Mac_Test_Harness_Disp
     /* ################################################################## */
     /**
      */
-    func hostNameCallback(_ inControl: NSView?) {
+    func ttlCallback(_ inControl: NSView?) {
+        sendParameters["TTL"] = (inControl as? NSTextField)?.stringValue
+    }
+
+    /* ################################################################## */
+    /**
+     */
+    func nameCallback(_ inControl: NSView?) {
         sendParameters["Name"] = (inControl as? NSTextField)?.stringValue
     }
 
@@ -86,7 +93,17 @@ class RVS_ONVIF_Mac_Test_Harness_CoreDispatcher: RVS_ONVIF_Mac_Test_Harness_Disp
     /* ################################################################## */
     /**
      */
-    func ntpListCallback(_ inControl: NSView?) {
+    func dynDNSTypeCallback(_ inControl: NSView?) {
+        if let control = inControl as? NSSegmentedControl {
+            let type = control.label(forSegment: control.selectedSegment)
+            sendParameters["Type"] = type
+        }
+    }
+
+    /* ################################################################## */
+    /**
+     */
+    func dnsIPAddressListCallback(_ inControl: NSView?) {
         if let control = inControl as? NSSegmentedControl {
             sendParameters["FromDHCP"] = 1 == control.selectedSegment
         } else if let control = inControl as? NSTextField, !control.stringValue.isEmpty {
@@ -101,7 +118,42 @@ class RVS_ONVIF_Mac_Test_Harness_CoreDispatcher: RVS_ONVIF_Mac_Test_Harness_Disp
             })
             
             var ips: [[String: String]] = []
+            
+            adNames.ipAddresses.forEach {
+                let type = $0.isV6 ? "IPv6" : "IPv4"
+                let key = "tt:" + type + "Address"
+                let val: [String: String] = ["tt:Type": type, key: $0.address]
+                ips.append(val)
+            }
+            
+            for name in adNames.names {
+                let val: [String: String] = ["tt:Type": "DNS", "tt:DNSname": name]
+                ips.append(val)
+            }
+            
+            sendParameters["DNSManual"] = ips
+        }
+    }
 
+    /* ################################################################## */
+    /**
+     */
+    func ntpIPAddressListCallback(_ inControl: NSView?) {
+        if let control = inControl as? NSSegmentedControl {
+            sendParameters["FromDHCP"] = 1 == control.selectedSegment
+        } else if let control = inControl as? NSTextField, !control.stringValue.isEmpty {
+            let servers: [String] = control.stringValue.split(separator: ",").compactMap { return String($0) }
+            var adNames: (names: [String], ipAddresses: [RVS_IPAddress]) = (names: [String](), ipAddresses: [RVS_IPAddress]())
+            adNames = servers.reduce(into: adNames, { (adNames, nextVal) in
+                if nextVal.ipAddress?.isValidAddress ?? false {
+                    adNames.ipAddresses.append(nextVal.ipAddress!)
+                } else {
+                    adNames.names.append(nextVal)
+                }
+            })
+            
+            var ips: [[String: String]] = []
+            
             adNames.ipAddresses.forEach {
                 let type = $0.isV6 ? "IPv6" : "IPv4"
                 let key = "tt:" + type + "Address"
@@ -116,6 +168,13 @@ class RVS_ONVIF_Mac_Test_Harness_CoreDispatcher: RVS_ONVIF_Mac_Test_Harness_Disp
             
             sendParameters["NTPManual"] = ips
         }
+    }
+
+    /* ################################################################## */
+    /**
+     */
+    func dnsSearchDomainCallback(_ inControl: NSView?) {
+        sendParameters["SearchDomain"] = (inControl as? NSTextField)?.stringValue
     }
 
     /* ################################################################## */
