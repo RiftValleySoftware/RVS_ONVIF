4c99cd8a37eafbcf646f72fa481b37e2904ad373
Chris Marshall <chris@riftvalleysoftware.com>
2019-05-03 15:55:22 -0400

    Beginning the process of adding video preview to the Mac test harness. Updated the podfile.

---
+++ RVS_ONVIF Mac Test Harness/RVS_ONVIF_Mac_Test_Harness_VideoDisplayViewController.swift
@@ -0,0 +1,28 @@
+/**
+ © Copyright 2019, The Great Rift Valley Software Company. All rights reserved.
+ 
+ This code is proprietary and confidential code,
+ It is NOT to be reused or combined into any application,
+ unless done so, specifically under written license from The Great Rift Valley Software Company.
+ 
+ The Great Rift Valley Software Company: https://riftvalleysoftware.com
+ */
+
+import Cocoa
+import RVS_ONVIF_MacOS
+
+/* ################################################################################################################################## */
+// MARK: - The Main Video Display View Controller
+/* ################################################################################################################################## */
+class RVS_ONVIF_Mac_Test_Harness_VideoDisplayViewController: NSViewController {
+    /* ################################################################## */
+    /**
+     */
+    @IBOutlet weak var videoDisplayView: NSView!
+    
+    /* ################################################################## */
+    /**
+     */
+    override func viewDidLoad() {
+    }
+}
