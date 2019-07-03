0f1e0283b4fa2b6acec4a0631044a09f713cd0ff
Chris Marshall <chris@riftvalleysoftware.com>
2019-07-03 19:45:16 -0400

    Added the basics for a tvOS test harness app.

---
+++ RVS_ONVIF tvOS Test Harness/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RVS_ONVIF tvOS Test Harness
+//
+//  Created by Chris Marshall on 7/3/19.
+//  Copyright © 2019 The Great Rift Valley Software Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
