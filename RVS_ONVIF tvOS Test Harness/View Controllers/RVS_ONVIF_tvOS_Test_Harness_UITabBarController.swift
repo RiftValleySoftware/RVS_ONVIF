cff6ebc629f33de1c580881fd0b2e242baa5d28f
Chris Marshall <chris@riftvalleysoftware.com>
2019-07-08 16:54:11 -0400

    Reordered the files, and I think I'm ready to start work on the app, itself.

--- RVS_ONVIF tvOS Test Harness/View Controllers/RVS_ONVIF_tvOS_Test_Harness_UITabBarController.swift
+++ RVS_ONVIF tvOS Test Harness/View Controllers/RVS_ONVIF_tvOS_Test_Harness_UITabBarController.swift
@@ -10,10 +10,22 @@
 
 import UIKit
 
-class RVS_ONVIF_tvOS_Test_Harness_ViewController: UIViewController {
+class RVS_ONVIF_tvOS_Test_Harness_UITabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
