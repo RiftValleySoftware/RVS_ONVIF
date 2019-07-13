da5e48bcaa9a2d379cbe1442260308cc06fbd27f
Chris Marshall <chris@riftvalleysoftware.com>
2019-07-13 08:08:00 -0400

    Got the info screen showing properly. Next, namespaces...

--- RVS_ONVIF tvOS Test Harness/View Controllers/RVS_ONVIF_tvOS_Test_Harness_Info_ViewController.swift
+++ RVS_ONVIF tvOS Test Harness/View Controllers/RVS_ONVIF_tvOS_Test_Harness_Info_ViewController.swift
@@ -11,9 +11,17 @@
 import UIKit
 
 /* ################################################################################################################################## */
+// MARK: - Class for one cell of table data.
+/* ################################################################################################################################## */
+class RVS_ONVIF_tvOS_Test_Harness_Info_ViewController_ValueTableCellView: UITableViewCell {
+    @IBOutlet var keyLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+}
+
+/* ################################################################################################################################## */
 // MARK: - Main Class for the Info Screen
 /* ################################################################################################################################## */
-class RVS_ONVIF_tvOS_Test_Harness_Info_ViewController: RVS_ONVIF_tvOS_Test_Harness_Base_ViewController {
+class RVS_ONVIF_tvOS_Test_Harness_Info_ViewController: RVS_ONVIF_tvOS_Test_Harness_Base_ViewController, UITableViewDelegate, UITableViewDataSource {
     /* ############################################################################################################################## */
     // MARK: - Internal Instance Properties
     /* ############################################################################################################################## */
@@ -29,5 +37,34 @@ class RVS_ONVIF_tvOS_Test_Harness_Info_ViewController: RVS_ONVIF_tvOS_Test_Harne
      */
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    /* ############################################################################################################################## */
+    // MARK: - UITableViewDataSource Methods
+    /* ############################################################################################################################## */
+    /* ################################################################## */
+    /**
+     */
+    func tableView(_ inTableView: UITableView, numberOfRowsInSection inSection: Int) -> Int {
+        return onvifInstance?.core?.deviceInformation.count ?? 0
+    }
+    
+    /* ################################################################## */
+    /**
+     */
+    func tableView(_ inTableView: UITableView, cellForRowAt inIndexPath: IndexPath) -> UITableViewCell {
+        if let dictKeys = onvifInstance?.core?.deviceInformation.keys.sorted() {
+            let key = dictKeys[inIndexPath.row]
+            if let value = onvifInstance?.core?.deviceInformation[key] as? String {
+                if let ret = inTableView.dequeueReusableCell(withIdentifier: "InfoValueCell") as? RVS_ONVIF_tvOS_Test_Harness_Info_ViewController_ValueTableCellView {
+                    ret.keyLabel?.text = key + ":"
+                    ret.valueLabel?.text = value
+                    
+                    return ret
+                }
+            }
+        }
+        
+        return UITableViewCell()
     }
 }
