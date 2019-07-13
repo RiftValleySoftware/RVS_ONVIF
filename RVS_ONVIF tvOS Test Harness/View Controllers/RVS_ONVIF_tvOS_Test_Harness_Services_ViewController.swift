a2f083e5129da5563aa47293c1ee54697bfeb5a1
Chris Marshall <chris@riftvalleysoftware.com>
2019-07-13 12:49:47 -0400

    Added a shell for the Services tab. Messed around with the embedded libraries.

---
+++ RVS_ONVIF tvOS Test Harness/View Controllers/RVS_ONVIF_tvOS_Test_Harness_Services_ViewController.swift
@@ -0,0 +1,33 @@
+/**
+ © Copyright 2019, The Great Rift Valley Software Company. All rights reserved.
+ 
+ This code is proprietary and confidential code,
+ It is NOT to be reused or combined into any application,
+ unless done so, specifically under written license from The Great Rift Valley Software Company.
+ 
+ The Great Rift Valley Software Company: https://riftvalleysoftware.com
+ */
+
+import UIKit
+
+/* ################################################################################################################################## */
+// MARK: - Main Class for the Info Screen
+/* ################################################################################################################################## */
+class RVS_ONVIF_tvOS_Test_Harness_Services_ViewController: RVS_ONVIF_tvOS_Test_Harness_Base_ViewController {
+    /* ############################################################################################################################## */
+    // MARK: - Internal Instance Properties
+    /* ############################################################################################################################## */
+    /* ################################################################## */
+    /**
+     */
+    
+    /* ############################################################################################################################## */
+    // MARK: - Internal Base Class Override Methods
+    /* ############################################################################################################################## */
+    /* ################################################################## */
+    /**
+     */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
