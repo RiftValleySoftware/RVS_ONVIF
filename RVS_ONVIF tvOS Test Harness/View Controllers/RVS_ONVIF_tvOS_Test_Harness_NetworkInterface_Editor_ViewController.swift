46b945b7dc6c796559f7a9ff32b13c8b4b84ef89
Chris Marshall <chris@riftvalleysoftware.com>
2019-07-22 10:31:24 -0400

    Continuing to "flesh out" the rather extensive network interface editor.

--- RVS_ONVIF tvOS Test Harness/View Controllers/RVS_ONVIF_tvOS_Test_Harness_NetworkInterface_Editor_ViewController.swift
+++ RVS_ONVIF tvOS Test Harness/View Controllers/RVS_ONVIF_tvOS_Test_Harness_NetworkInterface_Editor_ViewController.swift
@@ -50,9 +50,29 @@ class RVS_ONVIF_tvOS_Test_Harness_NetworkInterface_Editor_ViewController: UIView
     @IBOutlet weak var operSpeedTextField: UITextField!
     
     @IBOutlet weak var ipv4StackView: UIStackView!
+    @IBOutlet weak var ipv4EnabledSegmentedSwitch: UISegmentedControl!
+    @IBOutlet weak var ipv4DHCPSegmentedSwitch: UISegmentedControl!
+    @IBOutlet weak var ipv4ManualAddressStackView: UIStackView!
+    @IBOutlet weak var ipv4ManualAddressEntryTextField: UITextField!
+    @IBOutlet weak var ipv4ManualPrefixLengthTextField: UITextField!
+    @IBOutlet weak var ipv4LinkLocalAddressLabel: UILabel!
+    @IBOutlet weak var ipv4FromDHCPAddressLabel: UILabel!
     
-    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var ipv6StackView: UIStackView!
+    @IBOutlet weak var ipv6EnabledSegmentedSwitch: UISegmentedControl!
+    @IBOutlet weak var ipv6AcceptRALabel: UILabel!
+    @IBOutlet weak var ipv6AcceptRouterAdvertSegmentedSwitch: UISegmentedControl!
+    @IBOutlet weak var ipv6DHCPSegmentedSwitch: UISegmentedControl!
+    @IBOutlet weak var ipv6ManualAddressStackView: UIStackView!
+    @IBOutlet weak var ipv6ManualAddressEntryTextField: UITextField!
+    @IBOutlet weak var ipv5ManualPrefixLengthTextField: UITextField!
+    @IBOutlet weak var ipv6LinkLocalAddressLabel: UILabel!
+    @IBOutlet weak var ipv6FromDHCPAddressLabel: UILabel!
+    @IBOutlet weak var ipv6FromRAAddressLabel: UILabel!
+
     @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var extensionButton: UIButton!
+    @IBOutlet weak var sendButton: UIButton!
     
     /* ############################################################################################################################## */
     // MARK: - IBAction Methods
@@ -71,6 +91,12 @@ class RVS_ONVIF_tvOS_Test_Harness_NetworkInterface_Editor_ViewController: UIView
         dismiss(animated: true, completion: nil)
     }
     
+    /* ################################################################## */
+    /**
+     */
+    @IBAction func extensionButtonHit(_ sender: Any) {
+    }
+
     /* ############################################################################################################################## */
     // MARK: - Base Class Override Methods
     /* ############################################################################################################################## */
@@ -80,6 +106,25 @@ class RVS_ONVIF_tvOS_Test_Harness_NetworkInterface_Editor_ViewController: UIView
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        infoStackView.isHidden = true
+        
+        linkStackView.isHidden = true
+        
+        ipv4StackView.isHidden = true
+        ipv4ManualAddressStackView.isHidden = true
+        ipv4LinkLocalAddressLabel.isHidden = true
+        ipv4FromDHCPAddressLabel.isHidden = true
+        
+        ipv6StackView.isHidden = true
+        ipv6AcceptRALabel.isHidden = true
+        ipv6AcceptRouterAdvertSegmentedSwitch.isHidden = true
+        ipv6ManualAddressStackView.isHidden = true
+        ipv6LinkLocalAddressLabel.isHidden = true
+        ipv6FromDHCPAddressLabel.isHidden = true
+        ipv6FromRAAddressLabel.isHidden = true
+        
+        extensionButton.isHidden = true
+
         if let networkInterfaceObject = networkInterfaceObject {
             enabledSegmentedSwitch.selectedSegmentIndex = networkInterfaceObject.isEnabled ? 0 : 1
             var tokenText = networkInterfaceObject.token.isEmpty ? "NO TOKEN" : networkInterfaceObject.token
@@ -87,14 +132,63 @@ class RVS_ONVIF_tvOS_Test_Harness_NetworkInterface_Editor_ViewController: UIView
                 tokenText += " (Hardware Address: \(info.hwAddress))"
             }
             tokenHeadlineLabel.text = tokenText
+
             if let info = networkInterfaceObject.info {
+                infoStackView.isHidden = false
                 nameTextField.text = info.name
                 mtuTextField.text = String(info.mtu)
             }
             
             if let link = networkInterfaceObject.link {
+                linkStackView.isHidden = false
                 interfaceTypeLabel.text = "Interface Type: " + String(describing: link.interfaceType)
+                adminAutoNegSegmentedSwitch.selectedSegmentIndex = link.adminSettings.autoNegotiation ? 0 : 1
+                adminDuplexSegmentedSwitch.selectedSegmentIndex = .Full == link.adminSettings.duplex ? 0 : 1
+                adminSpeedTextField.text = String(link.adminSettings.speed)
+                operAutoNegSegmentedSwitch.selectedSegmentIndex = link.operSettings.autoNegotiation ? 0 : 1
+                operDuplexSegmentedSwitch.selectedSegmentIndex = .Full == link.operSettings.duplex ? 0 : 1
+                operSpeedTextField.text = String(link.operSettings.speed)
             }
+            
+            if let ipv4 = networkInterfaceObject.ipV4 {
+                ipv4StackView.isHidden = false
+                if let manual = ipv4.configuration.manual {
+                    ipv4ManualAddressStackView.isHidden = false
+                    ipv4ManualAddressEntryTextField.text = manual[0].address.address
+                    ipv4ManualPrefixLengthTextField.text = String(manual[0].prefixLength)
+                }
+                
+                if nil != ipv4.configuration.linkLocal {
+                    ipv4LinkLocalAddressLabel.isHidden = false
+                }
+                
+                if nil != ipv4.configuration.fromDHCP {
+                    ipv4FromDHCPAddressLabel.isHidden = false
+                }
+            }
+            
+            if let ipv6 = networkInterfaceObject.ipV6 {
+                ipv4StackView.isHidden = false
+                if let manual = ipv6.configuration.manual {
+                    ipv6ManualAddressStackView.isHidden = false
+                    ipv6ManualAddressEntryTextField.text = manual[0].address.address
+                    ipv5ManualPrefixLengthTextField.text = String(manual[0].prefixLength)
+                }
+                
+                if nil != ipv6.configuration.linkLocal {
+                    ipv6LinkLocalAddressLabel.isHidden = false
+                }
+                
+                if nil != ipv6.configuration.fromDHCP {
+                    ipv6FromDHCPAddressLabel.isHidden = false
+                }
+                
+                if nil != ipv6.configuration.fromRA {
+                    ipv6FromRAAddressLabel.isHidden = false
+                }
+            }
+            
+            extensionButton.isHidden = nil == networkInterfaceObject.networkInterfaceExtension
         }
     }
 }
