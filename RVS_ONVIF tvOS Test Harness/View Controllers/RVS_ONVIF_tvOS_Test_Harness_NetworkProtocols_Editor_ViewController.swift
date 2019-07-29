ee4a7c7259b0efaadfb75d5d178b49c60643e0cf
Chris Marshall <chris@riftvalleysoftware.com>
2019-07-29 08:27:36 -0400

    OK. I have the protocols editor screen displaying properly. It still doesn't do anything, though...

--- RVS_ONVIF tvOS Test Harness/View Controllers/RVS_ONVIF_tvOS_Test_Harness_NetworkProtocols_Editor_ViewController.swift
+++ RVS_ONVIF tvOS Test Harness/View Controllers/RVS_ONVIF_tvOS_Test_Harness_NetworkProtocols_Editor_ViewController.swift
@@ -84,6 +84,70 @@ class RVS_ONVIF_tvOS_Test_Harness_NetworkProtocols_Editor_ViewController: UIView
     }
     
     /* ############################################################################################################################## */
+    // MARK: - Instance Methods
+    /* ############################################################################################################################## */
+    /* ################################################################## */
+    /**
+     */
+    func addProtocol(_ inType: RVS_ONVIF_Core.NetworkProtocol) {
+        var name = ""
+        var portInt = 0
+        var enabled = false
+        
+        switch inType {
+        case let .HTTP(port, isEnabled):
+            name = "HTTP"
+            portInt = port
+            enabled = isEnabled
+        case let .HTTPS(port, isEnabled):
+            name = "HTTPS"
+            portInt = port
+            enabled = isEnabled
+        case let .RTSP(port, isEnabled):
+            name = "RTSP"
+            portInt = port
+            enabled = isEnabled
+        }
+        
+        print("Add: \(name), Port: \(portInt), Enabled: \(enabled)")
+        
+        let containerView = UIStackView()
+        containerView.axis = .horizontal
+        containerView.spacing = 20
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+
+        let nameLabel = UILabel()
+        nameLabel.text = name
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 40)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        containerView.addArrangedSubview(nameLabel)
+
+        let portLabel = UILabel()
+        portLabel.text = "Port:"
+        portLabel.font = UIFont.boldSystemFont(ofSize: 40)
+        portLabel.textAlignment = .right
+        portLabel.setContentHuggingPriority(.required, for: .horizontal)
+        containerView.addArrangedSubview(portLabel)
+
+        let portTextField = UITextField()
+        portTextField.text = String(portInt)
+        portTextField.translatesAutoresizingMaskIntoConstraints = false
+        portTextField.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        containerView.addArrangedSubview(portTextField)
+
+        let enabledSegmentedView = UISegmentedControl(items: ["Enabled", "Disabled"])
+        enabledSegmentedView.selectedSegmentIndex = enabled ? 0 : 1
+        portLabel.setContentHuggingPriority(.required, for: .horizontal)
+        containerView.addArrangedSubview(enabledSegmentedView)
+        
+        containerView.addArrangedSubview(UIView())
+
+        mainStackView.addArrangedSubview(containerView)
+    }
+    
+    /* ############################################################################################################################## */
     // MARK: - Base Class Override Methods
     /* ############################################################################################################################## */
     /* ################################################################## */
@@ -91,5 +155,15 @@ class RVS_ONVIF_tvOS_Test_Harness_NetworkProtocols_Editor_ViewController: UIView
      */
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let networkProtocolsObjects = networkProtocolsObjects {
+            mainStackView.spacing = 20
+
+            networkProtocolsObjects.forEach {
+                addProtocol($0)
+            }
+            
+            mainStackView.addArrangedSubview(UIView())
+        }
     }
 }
